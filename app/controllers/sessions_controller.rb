ba885cc8ab485831ccabd8370c59a114eca95d60
dbhmunro <dbhmunro@gmail.com>
2020-12-09 21:45:06 -0500

    user sign in and out with JWT

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,9 @@ class SessionsController < ApplicationController
             else
                 render json: { message: "User not found, please try again"}
             end
-        elsif !!params[:user][:id] #will likely be recieving token here and need to decode it for userId
-            user = User.find_by(id: params[:user][:id])
+        elsif !!params[:user][:token] #will likely be recieving token here and need to decode it for userId
+            logged_in?
+            # user = User.find_by(id: params[:user][:id])
             token = encode_token({user_id: user.id})
             render json: {user: user, token: token}
             #would be good to add conditional in case token is no longer valid, so that the token can be deleted from localstorage
